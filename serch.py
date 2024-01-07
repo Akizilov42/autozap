8fb35ea0c42bda468764232f025e806a63434c78
ZaGa42 <akizilov42@gmail.com>
2024-01-07 20:16:03 +0700

    new3

--- serch.py
+++ serch.py
@@ -3,3 +3,27 @@ car="tesla"
 print(car)
 a="five"
 print(a)
+a=2+2
+print(a)
+a=10-5
+print(a)
+a=5*5
+print(a)
+a=10/2
+print(a)
+a=10//2
+print(a)
+a=(5+5)*3
+print(a)
+print('Теперь я знаю что такое строка в Python')
+print('это спатра')
+mesage='хьюстон у нас проблема'
+print(mesage)
+mesage='we are heve a problem'
+print(mesage)
+print('илон маск'+' отправил теслу на марс')
+name='Илон '
+print(name+mesage)
+print(f'Илон {mesage}')
+a=2>4
+print(a)
