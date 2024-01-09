853d3a657e098bc45ceedd295f393a42b62d443b
ZaGa42 <akizilov42@gmail.com>
2024-01-09 22:31:28 +0700

    finish 09.01.2024

---
+++ new0901.py
@@ -0,0 +1,10 @@
+value='Язык Python'
+print('Текст:', value)
+value=321
+print('Число:', value)
+name=input('Как вас зовут?')
+age=int(input('Сколько вам лет?'))
+print('Добрый день!', name+'!')
+print('Вы родились в', 2024-age,'году!')
+txt='2+2'
+print(txt,'=',eval(txt))
